8bc00f595a982c682bfd3734136c861abedfa2f5
julesparry <julianparry@gmail.com>
2011-05-21 19:32:56 +1000

    fixing everything

--- puppet/files/deploy.rb
+++ puppet/files/deploy.rb
@@ -11,7 +11,13 @@ module MCollective
 
       action "war" do
         validate :source, String
-
+	war_file  = request[:source]
+	%x{ wget #{war_file} -O /tmp/company-news.war } 
+	%x{ service tomcat6 stop }
+	%x{ rm -rf /var/lib/tomcat6/webapps/* }
+	%x{ mv /tmp/company-news.war /var/lib/tomcat6/webapps/ }
+	%x{ service tomcat6 start }
+	reply[:out] = 'ok, deployed.'
       end
     end
   end
--- puppet/manifests/app.pp
+++ puppet/manifests/app.pp
@@ -2,6 +2,24 @@ import "base.pp"
 
 class app inherits base {
 
+$prevayler_path = "/var/db/prevayler"
+
+package { "tomcat6":
+	ensure => "installed",
+}
+
+file { "/var/db/prevayler":
+	ensure => "directory",
+	owner => "tomcat",
+	require => Package["tomcat6"]
+}
+
+file { "/etc/tomcat6/tomcat6.conf": 
+	ensure => "present",
+	content => template("tomcat6.conf.erb"),
+	require => Package["tomcat6"],	
+}
+
 }
 
 include app
